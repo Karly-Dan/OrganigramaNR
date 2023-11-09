9cf807b6f8947232b13389a4107d30bacce898fe
vikoss <vikoss.yiss@gmail.com>
2023-11-09 15:21:49 -0600

    agregar configuraciones para el deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,5 +37,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  server: {
+    host: '0', // default: localhost
+    port: 4000, // default: 3000
+  },
 }
